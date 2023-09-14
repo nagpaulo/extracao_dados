b4f59c733864fbc08762d405b180f7dc3d36c8a9
Paulo Roberto <paulo.roberto@seduc.ce.gov.br>
2023-09-14 09:51:52 -0300

    Configurando Bearer Token

--- src/extracao_api_twitter.py
+++ src/extracao_api_twitter.py
@@ -1,4 +1,23 @@
 from datetime import datetime, timedelta
+import requests
+import os
+import json
 
-end_time = datetime.now()
-start_time = (datetime.now() + timedelta(-1)).date()
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.000Z"
+
+end_time = datetime.now().strftime(TIMESTAMP_FORMAT)
+start_time = (datetime.now() + timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)
+query = "data science"
+
+tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,lang,text"
+user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
+
+url_raw = f"https://labdados.com/2/tweets/search/recent?query={query}&{tweet_fields}&{user_fields}&start_time={start_time}&end_time={end_time}"
+
+bearer_token = os.environ.get("BEARER_TOKEN")
+headers = {"Authorization":"Bearer {}".format(bearer_token)}
+response = requests.request("GET", url_raw, headers=headers)
+
+json_response = response.json()
+ 
+print(json.dumps(json_response, indent=4, sort_keys=True))
