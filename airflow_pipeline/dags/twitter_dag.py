a98f128fce6d51db694f6a6e6b37fa2f8c9f9ffa
Paulo Roberto <paulo.roberto@seduc.ce.gov.br>
2023-09-23 16:35:31 -0300

    Arquitetura de Medalha

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -7,11 +7,19 @@ from operators.twitter_operator import TwitterOperator
 from os.path import join
 from airflow.utils.dates import days_ago
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from pathlib import Path
 
 with DAG(dag_id = "TwitterDAG", start_date=days_ago(30), schedule_interval="@daily") as dag:
+    BASE_FOLDER = join(
+        str(Path("~/Documentos").expanduser()),
+        "Desenvolvimento/Curso/Alura - Apache Airflow/Extração de dados/datalake/{stage}/twitter_datascience/{partition}",        
+    )
+
+    PARTITION_FOLDER_EXTRACT = "extract_date={{ datainterval_start.strftime('%Y-%m-%d') }}"
+    TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
+
     query = "datascience"
-    twitter_operator = TwitterOperator(file_path=join("datalake/twitter_datascience",
-                                        "extract_date={{ ds }}",
+    twitter_operator = TwitterOperator(file_path=join(BASE_FOLDER.format(stage="bronze", partion=PARTITION_FOLDER_EXTRACT),
                                         "datascience_{{ ds_nodash }}.json"), 
                             start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", 
                             end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", 
@@ -21,7 +29,7 @@ with DAG(dag_id = "TwitterDAG", start_date=days_ago(30), schedule_interval="@dai
     twitter_transform = SparkSubmitOperator(task_id="transform_twitter_datascience",
                                             application="src/spark/transformation.py",
                                             name="twitter_transformation",
-                                            application_args=["--src","datalake/twitter_datascience", 
-                                                              "--dest", "dados_transformation",  
+                                            application_args=["--src", BASE_FOLDER.format(stage="bronze", partion=PARTITION_FOLDER_EXTRACT), 
+                                                              "--dest", BASE_FOLDER.format(stage="silver", partion=""),  
                                                               "--process-date", "{{ ds }}"])
 twitter_operator >> twitter_transform
